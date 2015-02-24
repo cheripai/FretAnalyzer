4aec6f29256f43623b1833a1c1522f3432f8e660
pcammall <pcamm001@ucr.edu>
2015-02-24 14:21:57 -0800

    Updated parser to actually parse. But has inifinite loop issue. Likely on advancing to empty/NULL tokens.

--- src/fret_input.cpp
+++ src/fret_input.cpp
@@ -1,65 +1,172 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <utility>
+#include <fstream>
+#include <string>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
-int main()
+void print_vect(vector<vector<double> > v)
 {
-	fstream f;
-	cerr << "opeing file" << endl;
-	string file = "ex475 Rangep 1-5 uM UBC9 0-10uM.txt";
-	f.open(file);
-	cerr << "file opened" << endl;
-	string input;
-	
-
-	//eat the first two lines, which are stuff we don't care about
-	getline(f, input);
-	getline(f, input);
-	
-	char* token;
-	char* input_cstr = const_cast<char*>(input.c_str() );
-	string command = "python /FretAnalyzer/src/fret.py";
-	vector<vector<double> > fldd = 0.0;
-	vector<vector<double> > flaa = 0.0;
-	vector<vector<double> > emtotal=0.0;
-	vector<double> d;
-	fldd.push_back( d); //if i understand his correctly, we'll have 3 copies of the same empty vector
-	flaa.push_back( d ); //but once we start pushing stuff to them, they won't be copies anymore.
-	emtotal.push_back(d); //not pointers, so pushing to one won't affect the other.
-	
-	int col = 0, row = 0; //note, these are placeholders for the array to store the data in
-	while ( !f.eof() )
-	{
-		getline(f, input);
-		token = strtok(NULL, "\t");
-		//file ex414 has EM475, which is FLdd. file ex414 also has Em530, which is Emtotal
-		//file ex475 has EM530, whic his FLaa
-		if ( token == 'ex414')
-		{
-			token = strtok(NULL, "\t");
-			if (token == '475')
-			{
-				token = strtok(input_cstr, "\t"); //eats the temperature number
-				token = strtok(input_cstr, "\t"); //advances to the next one after the temperature number
-				while ( token != NULL)
-				{
-					token = strtok(input_cstr, "\t");
-					
-				}
-				if ( col != 0) //if other numbers were found after temperature, then we need to advance to the next line
-					row++;
-				col = 0;
-		//call python script using system, after appending on our information
-		
-	}
-	
-	return 0;
+    for ( unsigned x = 0; x < v.size(); ++x)
+    {
+        for (unsigned y = 0; y < v[x].size(); ++y)
+            cout << v[x][y] << "\t";
+        cout << endl;
+    }
 }
 
-		
-	
-	
+int main()
+{
+
+    fstream f;
+	cerr << "opeing file" << endl;
+	string file = "ex414 blank.txt";
+	f.open(file.c_str() );
+	cerr << "file opened" << endl;
+
+    string input;
+    vector< vector<double> > fldd, flaa, emTotal;
+    vector< double > d;
+
+    bool flag = false;
+    int null_count = 0;
+    char* token;
+    bool outer = true, inner = true, mid = true;
+    const char* tab = "\t";
+    char* cstr;
+    while ( outer )
+    {
+        getline(f, input);
+        cerr << "outerloop curr line : " << input << endl;
+        mid = true;
+        if ( file.find("ex414") == 0 ) //in the ex475 file, containing FLdd and EmTotal
+        {
+            getline(f, input);
+            cerr << "file find() curr line : " << input << endl;
+            if ( input.find("475") == 0 )
+            {
+                while ( mid ) //mid loop keeps parsing lines
+                {
+                    getline(f, input);
+                    cerr << "curr line : " << input << endl;
+                    cstr = const_cast<char*>(input.c_str() );
+                    while ( inner ) //inner loop keeps parsing tokens
+                    {
+                        token = strtok( NULL,  "\t");
+                        cerr << "current token: " << token << endl;
+                        if ( !token ) //not empty, not null, so filled with something
+                        {
+                            null_count++;
+                            if ( null_count >= 9) //if we've already eaten actaul numbers, but keep encountering empty stuff
+                            {   //then move on to finding emTotal in ex475
+                                inner = false;
+                            }
+                            if ( null_count >= 10)
+                                mid = false;
+                                
+                        }
+                        else    //add it to the array
+                        {
+                            d.push_back( strtod( const_cast<const char*>(token), NULL ) );
+                            flag = true;
+                            null_count = 0;
+                        }
+
+                            
+                    }//end while inner
+                    null_count = 0; //reset null_count for next line
+                    cerr << "pushing back" << endl;
+                    fldd.push_back(d); //add current line to fldd
+                    d.clear(); //erase everything in d so it can be reused
+                    inner = true;
+                    
+                }//end while mid
+            }//end if find(475)
+            else if ( input.find("530") == 0 )
+            {
+                while ( mid )
+                {
+                    getline(f, input);
+                    cstr = const_cast<char*>(input.c_str() );
+                    while ( inner )
+                    {
+                        token = strtok(cstr, tab);
+                        if ( !token)
+                        {
+                            null_count++;
+                            if ( null_count >= 9)
+                                inner = false;
+                            if ( null_count >= 10)
+                            {
+                                mid = false;
+                                outer = false;
+                            }
+                        }
+                        else
+                        {
+                            d.push_back( strtod( const_cast<const char*>(token), NULL ) );
+                            flag = true;
+                            null_count = 0;
+                        }
+                    }//end while inner
+
+                    null_count = 0;
+                    emTotal.push_back(d);
+                    d.clear();
+                    inner = true;
+                }//end while mid
+            }//end if find(530)
+
+        }//end if ex475
+
+        else if ( file.find("ex475") == 0 ) //ex475 has em530, which is flaa
+        {
+            if ( input.find("530") == 0 )
+            {
+                while ( mid ) //mid loop keeps parsing lines
+                {
+                    getline(f, input);
+                    cstr = const_cast<char*>(input.c_str() );
+                    while ( inner ) //inner loop keeps parsing tokens
+                    {
+                        token = strtok( NULL,  tab);
+                        if ( !token ) //not empty, not null, so filled with something
+                        {
+                            null_count++;
+                            if ( null_count >= 9) //if we've already eaten actaul numbers, but keep encountering empty stuff
+                            {   //then move on to finding emTotal in ex475
+                                inner = false;
+                            }
+                            if ( null_count >= 10)
+                            {
+                                mid = false;
+                                outer = false;
+                            }
+                        }
+                        else    //add it to the array
+                        {
+                            d.push_back( strtod( const_cast<const char*>(token), NULL ) );
+                            flag = true;
+                            null_count = 0;
+                        }
+
+                            
+                    }//end while inner
+                    null_count = 0; //reset null_count for next line
+                    flaa.push_back(d); //add current line to fldd
+                    d.clear(); //erase everything in d so it can be reused
+                    inner = true;
+                    
+                }//end while mid
+            }//end if find(530)
+        }//end else if
+    }//end while outer
+                    
+                            
+            
+    print_vect(fldd);
+
+    return 0;
+}
