b70f4892b4f57a5bf6c9c250a6b7090276f3739a
cheripai <datdo@rocketmail.com>
2015-05-10 15:35:57 -0700

    added labels to graphs

--- src/fret.py
+++ src/fret.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from decimal import *
 import json
 from lmfit import conf_interval, report_ci, fit_report, minimize, Parameters, Parameter
-from matplotlib.pyplot import plot, savefig
+from matplotlib.pyplot import plot, savefig, text
 from numpy import array, linspace, sqrt, zeros, reshape, average
 
 
@@ -17,15 +17,17 @@ def residuals(params, x, y, a):
     return y - emfret(params, x, a)
 
 
-def create_plot(filename, result, x, y, a):
+def create_plot(filename, result, x, y, a, i):
     xx = linspace(x.min(), x.max(), 50)         #so this returns 50 evenly spaced values between the start and stop points
     yy = emfret(result.params, xx, a)           #it uses each of these to calculate emfretmax (one per xx value)
     z = linspace(x.min(), x.max(), 50);
     #this plots all the values contained in Y
     plot(x, y, 'bo')
     #this plots the calcualted EMfret (xx,yy), and a black horizontal line through the origin (xx, z) for orientation of the axis
-    plot(xx, yy, 'g-', xx, z, 'k-')
-    savefig(filename, bbox_inches='tight', dpi = 400)
+    plot(xx, yy, 'g-')
+    plot(xx, z, 'k-')
+    text(xx[-1], yy[-1], '{}'.format(i+1), ha='left', position=(xx[-1]+0.05, yy[-1]))
+    savefig(filename, bbox_inches='tight', dpi=400)
 
 
 def init_argparse():
@@ -73,7 +75,6 @@ def main():
             y[i] = average(all_y, axis=0)
 
 
-
     #adding parameters, initial guesses, and constraints
     params = Parameters()
     params.add('kd', value=1, min=0)
@@ -82,7 +83,7 @@ def main():
     #run fitting procedure and display results
     #this needs to be repeated for each A value. Note that A[i] corresponds to Y[i]
     #X is assumed to hold constant across all of these, so it remains unchanged across iterations
-    for i in range (len(a)):
+    for i in range(len(a)):
         result = minimize(residuals, params, args=(x, y[i], a[i] ))
         ci = conf_interval(result, maxiter=1000)
 
@@ -93,7 +94,8 @@ def main():
             f.write(json.dumps(ci) + '\n')
             f.close()
         else:
-            for param_name in params: #prints to stdout. Needs to be modified
+            print('Y{}:\n'.format(i+1))
+            for param_name in params: 
                 print('{}:\n{}\n'.format(param_name, round(params[param_name].value, 4)))
                 p_width = max(len(str(p)) for p in ci[param_name][0])
                 getcontext().prec = 4
@@ -103,7 +105,7 @@ def main():
         
         # plots data and curve on graph and displays if output file is given
         if(args.plot):
-            create_plot(args.plot, result, x, y[i], a[i])
+            create_plot(args.plot, result, x, y[i], a[i], i)
     
 if __name__ == "__main__":
     main()
