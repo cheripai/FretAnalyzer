53c72f7d83e5f7410479a34077981a66e8ede479
cheripai <datdo@rocketmail.com>
2015-06-07 14:09:36 -0700

    Add display of R-Squared values

--- GUI/fret.py
+++ GUI/fret.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from decimal import *
 from lmfit import conf_interval, report_ci, fit_report, minimize, Parameters, Parameter
 from matplotlib.pyplot import plot, savefig, text, ticklabel_format, xlabel, ylabel
-from numpy import array, linspace, sqrt, zeros, reshape, average, std
+from numpy import array, linspace, sqrt, zeros, reshape, average, std, var
 
 
 def emfret(params, x, a):
@@ -117,6 +117,7 @@ def main():
 
         # Print results
         print('Concentration: {}\n'.format(a[i]))
+        print('R-Squared: {}\n'.format(1-result.residual.var() / var(y)))
         for param_name in params: 
             print('{}:\n{} ± {}\n'.format(param_name, round(params[param_name].value, 4), round(params[param_name].stderr, 4)))
             p_width = max(len(str(p)) for p in ci[param_name][0])
